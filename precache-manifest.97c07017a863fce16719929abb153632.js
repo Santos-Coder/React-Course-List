8daa802364e13105b003477b2535c619d677e9ce
santos <santosrecovery@gmail.com>
2020-04-26 17:40:32 +0200

    Updates

---
+++ precache-manifest.97c07017a863fce16719929abb153632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a195b8d13430627a5ff27357003e0f",
+    "url": "/React-Course-List/index.html"
+  },
+  {
+    "revision": "f244fe1d23447a39865c",
+    "url": "/React-Course-List/static/css/main.dd31bc85.chunk.css"
+  },
+  {
+    "revision": "8ace2c108539302d6c7d",
+    "url": "/React-Course-List/static/js/2.401377c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Course-List/static/js/2.401377c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f244fe1d23447a39865c",
+    "url": "/React-Course-List/static/js/main.e5c501a3.chunk.js"
+  },
+  {
+    "revision": "ea041a2293dbebf8cce9",
+    "url": "/React-Course-List/static/js/runtime-main.dbbd7376.js"
+  }
+]);
